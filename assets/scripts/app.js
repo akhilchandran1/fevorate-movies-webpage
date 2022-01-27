278c0ec2be94462cdedf4d44e9be1b679df84c10
Akhil Chandran <akhil9216@gmail.com>
2022-01-27 01:18:05 +0000

    rendering item and delete model added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,6 +5,8 @@ const canselAddMovieButton = addMovieModel.querySelector('.btn--passive');
 const confirmAddMovieButton = addMovieModel.querySelector('.btn--success');
 const userInputs = addMovieModel.querySelectorAll('input');
 const entryTextSection = document.getElementById('entry-text');
+const listRoot = document.getElementById('movie-list');
+const deleteMovieModel = document.getElementById('delete-modal');
 
 const movies = [];
 
@@ -16,17 +18,61 @@ const updateUI = () => {
     }
 };
 
+const deleteMovie = movieId => {
+    let movieIndex = 0;
+    for (let movie of movies){
+        if (movie.id === movieId) {
+            break;
+        }
+        movieIndex++;
+    }
+    movies.splice(movieIndex, 1);
+    listRoot.children[movieIndex].remove();
+}
+
+const closeMovieDeletionModel = () => {
+    toggleBackdrop();
+    deleteMovieModel.classList.remove('visible');
+};
+
+const deleteMovieHandler = (movieId) => {
+    deleteMovieModel.classList.add('visible');
+    toggleBackdrop();
+    //deleteMovie(movieId);
+};
+
+const renderNewMovieElement = (id, title, imageUrl, rating) => {
+    const newMovieElement = document.createElement('li');
+    newMovieElement.className = 'movie-element';
+    newMovieElement.innerHTML = `
+        <div class = "movie-element__image">
+            <img src="${imageUrl}" alt=${title}>
+        </div>
+        <div class="movie-element__info">
+            <h2>${title}</h2>
+            <p>${rating}/5 stars</p>
+        </div>
+        `;
+    newMovieElement.addEventListener('click', deleteMovieHandler.bind(null, id));
+    listRoot.append(newMovieElement);
+};
+
 const toggleBackdrop = () => {
     backdrop.classList.toggle('visible');
 };
 
-const toggleMovieModel = () => {
-    addMovieModel.classList.toggle('visible');
+const closeMovieModel = () => {
+    addMovieModel.classList.remove('visible');
+};
+
+const showMovieModel = () => {
+    addMovieModel.classList.add('visible');
     toggleBackdrop();
 };
 
 const backdropClickHandler = () => {
-    toggleMovieModel();
+    closeMovieModel();
+    closeMovieDeletionModel();
 }
 
 const clearMovieInput = () => {
@@ -36,7 +82,7 @@ const clearMovieInput = () => {
 };
 
 const canselAddMovieHandler = () => {
-    toggleMovieModel();
+    closeMovieModel();
     clearMovieInput();
 };
 
@@ -56,18 +102,21 @@ const addMovieHandler = () => {
     }
 
     const newMovie = {
+        id: Math.random() * Date.now(), //its not recommented.
         title: titleValue,
         image: imageUrlValue,
         rating: ratingValue
     };
     movies.push(newMovie);
     console.log(movies);
-    toggleMovieModel();
+    closeMovieModel();
+    toggleBackdrop();
     clearMovieInput();
+    renderNewMovieElement(newMovie.id, newMovie.title, newMovie.image, newMovie.rating);
     updateUI();
 };
 
-startAddMovieButton.addEventListener('click', toggleMovieModel);
+startAddMovieButton.addEventListener('click', showMovieModel);
 backdrop.addEventListener('click', backdropClickHandler);
 canselAddMovieButton.addEventListener('click', canselAddMovieHandler);
 confirmAddMovieButton.addEventListener('click', addMovieHandler);
